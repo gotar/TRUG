66363cfac3dc9af161aac9b8f674430bc44af673
Oskar Szrajer <oskarszrajer@gmail.com>
2013-05-09 10:29:02 +0200

    fix test

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -9,9 +9,14 @@ describe "application" do
       last_response.should be_ok
     end
 
-    it "should contain Hello!" do
+    it "should contain ,,Dane'' section" do
       get '/'
-      last_response.body.should match /hello!/i
+      last_response.body.should match /dane/i
+    end
+
+    it "should contain ,,Lokalizacja'' section" do
+      get '/'
+      last_response.body.should match /lokalizacja/i
     end
   end
 end
