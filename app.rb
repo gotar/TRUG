edb4dc1be45dc24ddfce0f1306bb6626f1b9c1e7
Maciej Malecki <smt116@gmail.com>
2014-01-18 15:26:25 +0100

    Serve js via sinatra-asset-pack.

--- app.rb
+++ app.rb
@@ -7,12 +7,17 @@ class App < Sinatra::Base
 
   assets do
     serve '/css', from: 'public/css'
+    serve '/js', from: 'public/js'
 
     css :styles, '/css/styles.css', [
       '/css/custom.css',
       '/css/custom-mailchimp.css'
     ]
     css_compression :less
+
+    js :load_google_map, '/js/application.js', [
+      '/js/map.js'
+    ]
 
     prebuild true
     expires 86400 * 365, :public
