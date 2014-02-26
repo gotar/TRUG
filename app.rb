9796981cf1cb01dc950f740e681d928f8f9d561e
Adam Okoń <adam@okon.eu>
2014-02-26 09:48:15 +0100

    Cleaned up the talk view using by view helpers

--- app.rb
+++ app.rb
@@ -13,6 +13,22 @@ class App < Sinatra::Base
       header.push("(#{meetup[:date]})") if meetup[:date]
       header.join(' ')
     end
+
+    def talk_title(talk)
+      if talk[:slides]
+        "<a href='#{talk[:slides]}' target='_blank'>#{talk[:title]}</a>"
+      else
+        talk[:title]
+      end
+    end
+
+    def talk_speaker(talk)
+      if talk[:home_page]
+        "<a href='#{talk[:home_page]}' target='_blank'>#{talk[:fullname]}</a>"
+      else
+        talk[:fullname]
+      end
+    end
   end
 
   assets do
