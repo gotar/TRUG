58e38bca8d18b877b7857c938e999bd21f8bdfa5
Oskar Szrajer <oskarszrajer@gmail.com>
2017-08-29 10:49:01 +0200

    polish deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,6 @@ lock '3.4.0'
 set :application, 'TRUG'
 set :repo_url, 'https://github.com/gotar/TRUG/'
 
-set :deploy_to, '/home/gotar/Sites/TRUG'
+set :deploy_to, '/home/trug/Site'
 
 set :passenger_restart_with_touch, true
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :app, %w{ovh}
-role :web, %w{ovh}
-role :db,  %w{ovh}
+role :app, %w{trug@trug.pl}
+role :web, %w{trug@trug.pl}
+role :db,  %w{trug@trug.pl}
 
 set :rvm_type, :auto
 set :rvm_ruby_version, '2.3.3'
