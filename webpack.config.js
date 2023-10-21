ed82b90e1bdf748703730c241044108ce30d5563
Oskar Szrajer <oskarszrajer@gmail.com>
2023-10-21 22:13:27 +0200

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,76 @@
 const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 var webpack = require("webpack");
 
-module.exports = function (env, argv) {
-  return {
-    entry: {
-      site: ["./assets/index.js", "./assets/index.scss", "./assets/fontello.css"],
-      archive: "./assets/archive.js",
-    },
-    output: {
-      filename: "[name].[hash:8].js",
-      path: __dirname + "/build/assets",
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: "[name].[hash:8].css",
-        chunkFilename: "[id].css",
-      }),
-      new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-      }),
-      new WebpackManifestPlugin({
-        fileName: "manifest.json",
-        publicPath: "/assets/",
-        writeToFileEmit: true,
-      }),
+module.exports = (env, argv) => ({
+  devtool: argv.mode === "production" ? false : "source-map",
+  entry: {
+    site: ["./assets/index.js", "./assets/index.scss", "./assets/fontello.css"],
+    archive: "./assets/archive.js",
+  },
+  output: {
+    filename: "[name].[hash:8].js",
+    path: __dirname + "/build/assets",
+    clean: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash:8].css",
+      chunkFilename: "[id].css",
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new WebpackManifestPlugin({
+      fileName: "manifest.json",
+      publicPath: "/assets/",
+      writeToFileEmit: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/,
+        type: "asset/resource",
+      },
     ],
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
-        },
-        {
-          test: /.s?css$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
-        },
-        {
-          test: /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "[name].[hash:8].[ext]",
-                useRelativePath: true,
-              },
-            },
-          ],
-        },
-      ],
+  },
+  optimization: {
+    // minimize: argv.mode === "production" ? true : false,
+    // minimizer: [
+    //   new TerserPlugin({
+    //     extractComments: false,
+    //   }),
+    //   new CssMinimizerPlugin({
+    //     minimizerOptions: {
+    //       preset: [
+    //         "default",
+    //         {
+    //           discardComments: { removeAll: true },
+    //         },
+    //       ],
+    //     },
+    //   }),
+    // ],
+  },
+  resolve: {
+    alias: {
+      jquery: "jquery/src/jquery",
     },
-    optimization: {
-      minimizer: [new CssMinimizerPlugin({})],
-    },
-  };
-};
+  },
+  externals: {
+    jquery: "jQuery",
+  },
+});
