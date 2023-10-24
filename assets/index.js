f7ad95893c5497e130bf5de6b5a82a53b4f99210
Oskar Szrajer <oskarszrajer@gmail.com>
2023-10-24 08:37:43 +0200

    WIP

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,7 @@
-document.querySelector(".js-btn-schedule").addEventListener("click", function (e) {
-  e.preventDefault();
-  var offset = document.querySelector(".js-landing-schedule").offsetTop;
-  window.scrollTo({ top: offset, behavior: "smooth" });
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".js-btn-schedule").addEventListener("click", function (e) {
+    e.preventDefault();
+    var offset = document.querySelector(".js-landing-schedule").offsetTop;
+    window.scrollTo({ top: offset, behavior: "smooth" });
+  });
 });
